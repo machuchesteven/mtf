cabac61b450f9163e41dc649c805b291288e6fe7
machuchesteven <machuchesteven@gmail.com>
2022-06-30 10:20:36 +0300

    Skeleton Advancement

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import './App.css';
-import { ChakraProvider, Text, useDisclosure, Box, Grid, GridItem, Stack, Skeleton, Button, FormControl, FormLabel, Input, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, Heading } from '@chakra-ui/react'
+import { ChakraProvider, Text, Center, useDisclosure, Box, Grid, GridItem, Stack, Skeleton, Button, FormControl, FormLabel, Input, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, Heading } from '@chakra-ui/react'
 
 const Navigator = () => {
   return <div>
@@ -19,19 +19,16 @@ const Fold = () => {
     <Text bgGradient="linear(to-l, #7928CA, #FF0080)" bgClip="text" fontWeight="bold" fontSize="6xl">{headline}</Text>
     <Box>
       <Grid gap={2} borderRadius={2} templateColumns="repeat(4, 1fr)" m={2}>
-        <GridItem colSpan={1}>
+        <GridItem colSpan={2}>
           <Stack >
-            <Skeleton height="200px" />
-          </Stack>
-        </GridItem>
-        <GridItem colSpan={1}>
-          <Stack >
-            <Skeleton height="200px" />
+            <Skeleton height="600px" shadow="sm" />
+
           </Stack>
         </GridItem>
         <GridItem colSpan={2}>
           <Stack >
-            <Skeleton height="200px" />
+            <Text align="center">Explanation and Visuals for The Fold</Text>
+            <Skeleton height="500px" m="auto" />
           </Stack>
         </GridItem>
       </Grid>
@@ -52,12 +49,48 @@ const Fold = () => {
 
 const OurServices = () => {
   return <div>
-    <Heading>Our Services To The Society For development</Heading>
-
-    <p>The List and Point for Services To The Society</p>
-    <br />
-    <Skeleton height='40px' />
-    <p>We shall also have The Explaining Newsletter for New Memebers To Read about The Forum</p>
+    <Box m={5}>
+      <Center m={5} bg="teal.200" p={5} border="1px" borderRadius={5}>
+        <Heading>Our Services To The Society For development</Heading>
+      </Center>
+      <p>The List and Point for Services To The Society</p>
+      <br />
+      <Grid templateColumns="repeat(2, 1fr)" gap={2} m={5}>
+        <GridItem colSpan={1}>
+          <Stack >
+            <Skeleton height="300px" />
+          </Stack>
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Box marginY="auto">
+            <Center>
+              <Heading as="h4">Proffessional Training</Heading>
+            </Center>
+            <Text fontWeight="400" m={5}>We are providing proffessional trainings to both members of Our Forum and Non members of the Forum, like Cv writings, Presentation preparing, skills explaining and Interview skills to proffesions</Text>
+            <Center><Button>See More Information</Button></Center>
+          </Box>
+        </GridItem>
+      </Grid>
+      <Grid templateColumns="repeat(5, 1fr)" gap={2} maxWidth="300px" marginX="auto" marginY="2px">
+        <GridItem colSpan={1}>
+          <Skeleton height="2px" />
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Skeleton height="2px" />
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Skeleton height="5px" />
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Skeleton height="2px" />
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Skeleton height="2px" />
+        </GridItem>
+      </Grid>
+      <Skeleton height='200px' />
+      <p>We shall also have The Explaining Newsletter for New Memebers To Read about The Forum</p>
+    </Box>
   </div>
 }
 
@@ -160,24 +193,23 @@ function InitialFocus() {
 function App() {
   return (
     <ChakraProvider >
-      <div>
-        <Navigator />
-        <Fold />
-        <p>maendeleo talk forum official page under construction</p>
-        <OurServices />
-        <Upcoming />
-        <OurTeam />
-        <Impact />
-        <FounderNote />
-        <JoinUs />
-        <InitialFocus />
-        <PageFooter />
-      </div>
+      <Box maxWidth="1200px" align="center" m="auto">
+        <div>
+          <Navigator />
+          <Fold />
+          <p>maendeleo talk forum official page under construction</p>
+          <OurServices />
+          <Upcoming />
+          <OurTeam />
+          <Impact />
+          <FounderNote />
+          <JoinUs />
+          <InitialFocus />
+          <PageFooter />
+        </div>
+      </Box>
     </ChakraProvider>
   );
 }
 
 export default App;
-
-
-
